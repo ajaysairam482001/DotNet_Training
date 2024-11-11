56b55c02921f11b1d9b9e058a41c84c43d76773f
Ajay Sairam <ajaysa@infinite.com>
2024-11-11 10:22:46 +0530

    changes Q4

--- CSharp Assignments/Assignment4/Question4.cs
+++ CSharp Assignments/Assignment4/Question4.cs
@@ -40,9 +40,26 @@ namespace Ajay_Assignments.Assignment4
     {
         public static void Main(string[] args)
         {
-            PartTimeEmployee pt = new PartTimeEmployee(123,"ajay",123.23f,125.45f);
+          //  PartTimeEmployee pt = new PartTimeEmployee(123,"ajay",123.23f,125.45f);
+          Question4 question4 = new Question4();
+            PartTimeEmployee pt = question4.menu();
             pt.display();
             Console.Read();
+        }
+
+        PartTimeEmployee menu()
+        {
+            Console.WriteLine("Enter the Details below");
+            Console.Write("Enter ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Salary: ");
+            float salary = float.Parse(Console.ReadLine());
+            Console.Write("Enter Wages: ");
+            float wage = float.Parse(Console.ReadLine());
+            PartTimeEmployee pt = new PartTimeEmployee(id,name,salary,wage);
+            return pt;
         }
     }
 }
